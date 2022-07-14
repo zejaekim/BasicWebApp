c44aea7f6414daf6231293ef39e1b6fcbed6433f
jaewookim <ge47tar@mytum.de>
2022-07-14 11:09:04 +0200

    Return value for query name changed

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -12,7 +12,7 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "MyTeam";
+           return "Jae";
         } else { // TODO extend the programm here
             return "";
         }
